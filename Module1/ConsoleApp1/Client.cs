604dd360386d9175ae5c7305ed99f726ffbbe83c
unknown <moldir.polat@yandex.com>
2021-07-14 21:32:07 +0600

    module1-task1

---
+++ Module1/ConsoleApp1/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using myStringer;
+
+class MainClientApp
+{
+    // Static method Main is the entry point method.
+    public static void Main()
+    {
+        Stringer myStringInstance = new Stringer();
+        Console.WriteLine("Client code executes");
+        myStringInstance.StringerMethod();
+    }
+}
---
+++ Module1/ConsoleApp1/Stringer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myStringer
+{
+    public class Stringer
+    {
+        public void StringerMethod()
+        {
+            System.Console.WriteLine("This is a line from StringerMethod.");
+        }
+    }
+}
